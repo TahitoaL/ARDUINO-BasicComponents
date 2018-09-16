38010b5e02e3b8ea172e6a3f7a621ca0cbd8f18c
TahitoaL <feed.toal@gmail.com>
2018-09-16 22:26:30 +0200

    BasicDuration

---
+++ BasicComponents/core/BasicDuration.cpp
@@ -0,0 +1,47 @@
+#include "Arduino.h"
+#include "BasicDuration.h"
+
+BasicDuration::BasicDuration()
+{
+
+}
+
+BasicDuration& BasicDuration::operator+=(BasicDuration const& duration)
+{
+
+}
+
+bool BasicDuration::isEqual(BasicDuration const& b)
+{
+
+}
+
+bool BasicDuration::isInferiorTo(BasicDuration const& b)
+{
+
+}
+
+int BasicDuration::getHours()
+{
+
+}
+
+int BasicDuration::getMinutes()
+{
+
+}
+
+int BasicDuration::getSeconds()
+{
+
+}
+
+int BasicDuration::getMilliseconds()
+{
+
+}
+
+int BasicDuration::getDurationTime()
+{
+    
+}
