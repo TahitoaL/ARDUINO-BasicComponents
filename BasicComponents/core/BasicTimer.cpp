edbbad5f0856c5ffca6fc2d041bfbd771e7fa8f7
TahitoaL <feed.toal@gmail.com>
2018-11-24 20:33:35 +0100

    Implement BasicTimer

--- BasicComponents/core/BasicTimer.cpp
+++ BasicComponents/core/BasicTimer.cpp
@@ -3,20 +3,23 @@
 
 BasicTimer::BasicTimer(BasicDuration const& duration)
 {
-
+    _duration = duration;
 }
 
 void BasicTimer::init()
 {
-
+    _timeStart = millis();
 }
 
 bool BasicTimer::timeIsUp()
 {
-
+    if (millis() - _timeStart) >= _duration.getDurationTime()
+        return true;
+    else
+        return false;
 }
 
 BasicDuration& BasicTimer::getDuration()
 {
-    
+    return _duration;
 }
