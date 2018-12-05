dc860d8351996681f176fc740ec69a2b6afd594f
TahitoaL <feed.toal@gmail.com>
2018-12-05 19:57:19 +0100

    create library.properties

--- ARDUINO-BasicComponents.ino
+++ ARDUINO-BasicComponents.ino
@@ -1,13 +1,2 @@
+#include "Arduino.h"
 #include "./BasicComponents/BasicComponents.h"
-
-void setup()
-{
-    Serial.begin(9600);
-    Serial.println("Bonjour je fonctionne");
-}
-
-void loop()
-{
-    Serial.println("On a fait une boucle");
-    delay(1000);
-}
