e3c699132ba2670536a4d618383cb1f87eab1c99
TahitoaL <feed.toal@gmail.com>
2019-03-17 16:36:56 +0100

    Empty ino file

--- ARDUINO-BasicComponents.ino
+++ ARDUINO-BasicComponents.ino
@@ -1,23 +1,13 @@
 #include "Arduino.h"
 #include "./src/BasicComponents.h"
-
-BasicTimer timer1;
-BasicDuration time1(4);
-ColorSensor color1(8, 4, 5, 6, 7, 9, 0, "ColorSensor");
+#include "Streaming.h"
 
 void setup()
 {
-    Serial.begin(9600);
-    Serial.println("Lancement");
-    timer1.setValue(time1);
-    timer1.init();
-    color1.setUp();
+    
 }
 
 void loop()
 {
-    color1.switchOnLed();
-    delay(2000);
-    color1.switchOffLed();
-    delay(2000);
+
 }
