1fab4480b60f851d99ffe49d867341d101c7a285
TahitoaL <36546314+TahitoaL@users.noreply.github.com>
2019-04-24 11:00:07 +0200

    Add max and min PWM

--- src/core/BasicReceiverImplementation.cpp
+++ src/core/BasicReceiverImplementation.cpp
@@ -2,16 +2,18 @@
 #include "./../BasicComponents.h"
 #include "Streaming.h"
 
-BasicMotor::BasicMotor(char pin, int id, String name) : PwmReceiver(pin, id, name, 0)
+BasicMotor::BasicMotor(char pin, int id, String name, int minPwm = 0, int mawPwm = 255) : PwmReceiver(pin, id, name, 0)
 {
     _speed = 0;
+    _minPwm = minPwm;
+    _maxPwm = maxPwm;
 }
 
 void BasicMotor::setSpeed(int speed) // form 0 to 100
 {
     _speed = speed;
     Serial << _name << " : " << _dutyCycleValue << endl;
-    setDutyCycleValue(map(_speed, 0, 100, 20, 255));
+    setDutyCycleValue(map(_speed, 0, 100, _minPwm, _maxPwm));
 }
 
 int BasicMotor::getSpeed()
