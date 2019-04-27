13ef30244dae8bbc46b05f6ce0e8e0f881e98b43
TahitoaL <36546314+TahitoaL@users.noreply.github.com>
2019-04-27 12:55:37 +0200

    FixIssue

--- src/core/BasicReceiverImplementation.cpp
+++ src/core/BasicReceiverImplementation.cpp
@@ -2,7 +2,7 @@
 #include "./../BasicComponents.h"
 #include "Streaming.h"
 
-BasicMotor::BasicMotor(char pin, int id, String name, int minPwm = 0, int mawPwm = 255) : PwmReceiver(pin, id, name, 0)
+BasicMotor::BasicMotor(char pin, int id, String name, int minPwm = 0, int maxPwm = 255) : PwmReceiver(pin, id, name, 0)
 {
     _speed = 0;
     _minPwm = minPwm;
@@ -15,12 +15,12 @@ void BasicMotor::setSpeed(int speed) // from 0 to 100
     if (speed == 0) //Stop Motor
     {
         Serial << _name << " : " << 0 << endl;
-        setDutyCycleValue();
+        setDutyCycleValue(0);
     }
     else
     {
-        name << " : " << _dutyCycleValue << endl;
-        setDutyCycle(map(_speed, 0, 100, _minPwm, _maxPwm))
+        Serial << _name << " : " << _dutyCycleValue << endl;
+        setDutyCycleValue(map(_speed, 0, 100, _minPwm, _maxPwm));
     }
     
 }
